378d120bc5a2c042640deebe50f3fc9a8ee9ed50
luisfl04 <luisfelipecontato08@gmail.com>
2025-09-02 22:16:49 -0300

    Renderização de tabela interatica com os dados classificados implementada em versão inicial.

--- exibicao_processamento_noticias.py
+++ exibicao_processamento_noticias.py
@@ -4,7 +4,8 @@ import matplotlib.pyplot as plt
 from collections import Counter
 import nltk
 from nltk.corpus import stopwords
-
+from wordcloud import WordCloud
+import pandas as pd
 
 
 
@@ -17,19 +18,28 @@ class ExibicaoProcessamentoNoticias:
 
     def exibir_dados(self):
         # Obtendo data Frame:
-        processador_noticias = ProcessadorNoticias()
-        data_frame_noticias = processador_noticias.obter_df_noticias()
+        # processador_noticias = ProcessadorNoticias()
+        # data_frame_noticias = processador_noticias.obter_df_noticias()
         
-        st.title("Dashboard - Análise de Sentimento de Notíciais")
+        data_frame_noticias = pd.read_csv("classificacao_sentimentos_noticias.csv")
+        
+        # configs básicas do dashboard:
+        st.title("Dashboard - Classificação de sentimentos de noticias")
+
+        # ****** Tabela com os dados classificados ********
+        st.text("Tabela com as noticias classificadas", width=500)
+        st.dataframe(data_frame_noticias)
+        
 
         # ***** Gráfico de Pizza ******
 
         # Criando gráfico de pizza com a partir da contagem das classificações de sentimento:
         contagem_sentimentos = data_frame_noticias["classificacao_sentimento"].value_counts()
-        grafico_pizza, ax = plt.subplots()
-        ax.pie(contagem_sentimentos.values, labels=contagem_sentimentos.index, autopct="%1.1f%%", startangle=90)
-        ax.axis("equal")
-
+        grafico_pizza, axes_grafico_pizza = plt.subplots()
+        axes_grafico_pizza.pie(contagem_sentimentos.values, labels=contagem_sentimentos.index, autopct="%1.1f%%", startangle=90)
+        axes_grafico_pizza.axis("equal")
+        st.title("Grafico de pizza")     
+        st.pyplot(grafico_pizza)
 
         # ****** Nuvem de Palavras ******
 
@@ -45,15 +55,17 @@ class ExibicaoProcessamentoNoticias:
         # Obtendo palavras filtradas:
         lista_palavras_filtradas = [palavra for palavra in lista_palavras if palavra not in palavras_removiveis]
         contagem_frequencia_palavras = Counter(lista_palavras_filtradas)
-        palavras_mais_comuns = contagem_frequencia_palavras.most_common(10)
 
         # Exibindo nuvem de palavras:
-        wordcloud = WordCloud(width=800, height=400, background_color="white").generate_from_frequencies(frequencias)
+        nuvem_palavras = WordCloud(width=800, height=400, background_color="white").generate_from_frequencies(contagem_frequencia_palavras)
+        figura_nuvem_palavras, axes_nuvem_palavras = plt.subplots(figsize=(10, 5))
+        axes_nuvem_palavras.imshow(nuvem_palavras, interpolation="bilinear")
+        axes_nuvem_palavras.axis("off")
+        st.title("Nuvem de palavras frequêntes")
+        st.pyplot(figura_nuvem_palavras)
 
 
 
-                            
-        # st.pyplot(grafico_pizza)
     
 
 
